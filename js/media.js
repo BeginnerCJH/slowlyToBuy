d2183781bab4d26544337f24b9e46e8b3e23a0d2
qlj1997 <211043494@qq.com>
2018-11-01 21:05:57 +0800

    公共类

---
+++ js/media.js
@@ -0,0 +1,16 @@
+onload = function() {
+    font();
+    gotop();
+}
+window.onresize = function() {
+    font();
+}
+
+function font() {
+    var width = document.body.offsetWidth;
+    var pageWidth = 640;
+    var baseVal = 100;
+    var fz = width * baseVal / pageWidth;
+
+    document.querySelector('html').style.fontSize = fz + "px";
+}
